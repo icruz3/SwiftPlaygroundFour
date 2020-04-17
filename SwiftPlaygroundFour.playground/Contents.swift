a899377bd3d0c198799c7a8ccdbffce5664ec815
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-04-16 14:11:21 -1000

    Completed learning part 1-8

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 3 for <constant> in <range>:
+
+//for number in  1...5 {
+   // print (number)
+//}
+
+//part 4 section 6:
+var sum = 0
+
+//part 4 section 6 continued:
+for number in 1...5 {
+    sum += number
+// part 5 is the graph technique alone, the print belongs to part 4...
+    print (sum)
+}
+
+var i = 1 //added to resolve i, identification of part 6
+//Part 6 while loop <conditional statement> {}, code within
+while i <= 10
+{
+    // we want to print i
+print (i)
+    // we want to add +1 to i for each iteration through the loop
+    i = i + 1
+}
+
+
